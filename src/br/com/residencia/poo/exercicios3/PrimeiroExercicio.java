1123ab092fb181c4774305c957999f28aa41f18c
Gabriel Biti <gabriel-pms@hotmail.com>
2022-09-02 12:51:39 -0300

    Adição de alguns exercícios

---
+++ src/br/com/residencia/poo/exercicios3/PrimeiroExercicio.java
@@ -0,0 +1,18 @@
+package br.com.residencia.poo.exercicios3;
+
+public class PrimeiroExercicio {
+  public static void main(String[] args) {
+    int gastosJaneiro = 15000;
+    int gastosFevereiro = 23000;
+    int gastosMarco = 17000;
+    int gastosTrimestre;
+     
+   gastosTrimestre = gastosMarco + gastosFevereiro + gastosJaneiro;
+
+   System.out.println("O gasto total foi: " + gastosTrimestre);
+
+    
+
+
+  }
+}
